aef6d603bed2c82ec97519921f6039eb5784826a
Stephen <perdigon.johnstephen.bsit2020@gmail.com>
2023-10-16 20:44:28 +0800

    Test Commit

--- gabo/src/App.js
+++ gabo/src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         PUTANGINAMO
-         PUTANINGINAMO DIN
+         
         </p>
         <a
           className="App-link"
