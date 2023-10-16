6b6445cda107b2e4367ed98e45c48b650e6ca9f4
xealv <yrafontanilla14@gmail.com>
2023-10-16 17:01:36 +0800

    clonegit

--- gabo/src/App.js
+++ gabo/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          PUTANGINAMO
+         PUTANINGINAMO DIN
         </p>
         <a
           className="App-link"
